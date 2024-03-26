d9e3d4610fa86479b8611a1b9448588fed499739
Ryan-PG <heidarih313@gmail.com>
2024-03-26 15:58:47 +0330

    work on loading - not found - errors

---
+++ app/error.jsx
@@ -0,0 +1,10 @@
+"use client"
+const Errors = () => {
+    return(
+        <div className="flex justify-center items-center p-12"> 
+        خطایی رخ داد
+        </div>
+    )
+};
+
+export default Errors;
