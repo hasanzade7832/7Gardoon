f343fe4944756eab24b85c279333e89bda1648c8
hasanzade7832 <hasanzade.hoseyn27@gmail.com>
2024-04-19 23:43:27 +0330

    work on jalaseye45

--- app/page.jsx
+++ app/page.jsx
@@ -1,4 +1,4 @@
-// import MainSlider from "../components/mainSlider";
+import MainSlider from "../components/mainSlider";
 import ProductsSlider from "@/components/products-slider";
 import Categories from "@/components/graphic-cats";
 import Blogs from "@/components/blogs";
@@ -7,7 +7,7 @@ import Banners from "@/components/banners";
 const Home = () => {
   return (
     <div>
-      {/* <MainSlider /> */}
+      <MainSlider />
       <Categories />
       <ProductsSlider title="اپیکیشن ها" linkComp="/" />
       <ProductsSlider title="کتاب ها" linkComp="/" />
--- components/mainSlider/index.jsx
+++ components/mainSlider/index.jsx
@@ -1,84 +1,97 @@
-import { Swiper, SwiperSlide } from "swiper/react";
-import SwiperCore, {
-  Navigation,
-  Pagination,
-  Scrollbar,
-  Autoplay,
-} from "swiper";
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
-import "swiper/css/autoplay";
-SwiperCore.use([Autoplay]);
+"use client";
 
 import Image from "next/image";
+import Link from "next/link";
+import { FaChevronRight } from "react-icons/fa";
+import { FaChevronLeft } from "react-icons/fa";
 
-const SliderFour = () => {
+import { useState } from "react";
+import "animate.css";
+
+const MainSlider = () => {
+  const data = [
+    {
+      sorter: 0,
+      img: "/images/slider/slide1.jpg",
+      title:
+        "متن می‌باشد آنها با استفاده از محتویات ساختگی، صفحه گرافیکی خود را",
+      desc: "ست از متن‌های آزمایشی و  رعایت حق تکثیر متون را ندارند و در همان حال کار آنها به نوعی وابسته به متن می‌باشد آنها با استفاده از محتویات ساختگی، صفح",
+      slug: "product1",
+      price: "125000",
+    },
+    {
+      sorter: 1,
+      img: "/images/slider/slide2.jpg",
+      title: "چگونه در نظر گرفته شده‌است. از آنجایی که طراحان عموما نو",
+      desc: "که صفحه طراحی یا صفحه بندی شده بعد از اینکه متن در آن قرار گیرد چگونه به نظر می‌رسد و قلم‌ها و اندازه‌بندی‌ها چگونه در نظر گرفته شده‌است. از آنجایی که طراحان عموما نویسنده متن نیستند و وظیفه رعایت حق تکثیر متون را ندارند و در همان حال کار آنها به نوعی وابسته به متن می‌باشد آنها با استفاده از محتویات ساختگی، صفحه گرافیکی خود را صفحه‌آرایی می‌کنند تا مرح",
+      slug: "product2",
+      price: "260000",
+    },
+    {
+      sorter: 2,
+      img: "/images/slider/slide3.jpg",
+      title: "صرفا به مشتری یا صاحب کار خود نشان دهند که صفحه طراحی یا صفحه",
+      desc: "آنها به نوعی وابسته به متن می‌باشد آنها با استفاده از محتویات ساختگی، صفحه گرافیکی خود را آنها به نوعی وابسته به متن می‌باشد آنها با استفاده از محتویات ساختگی، صفحه گرافیکی خود را ",
+      slug: "product3",
+      price: "380000",
+    },
+  ];
+
+  const [nowSlide, setnowSlide] = useState(0);
+  const sliderChangingHandler = (inp) => {
+    let newNumber = nowSlide + inp;
+    if (newNumber > data.length - 1) {
+      newNumber = 0;
+    }
+    if (newNumber < 0) {
+      newNumber = data.length - 1;
+    }
+    setnowSlide(newNumber);
+  };
+  const [slideHandler, setslideHandler] = useState(1);
+
   return (
-    <section className="container mx-auto mt-7">
-      <Swiper
-        modules={[Navigation, Pagination, Scrollbar]}
-        spaceBetween={20}
-        slidesPerView={1}
-        navigation
-        pagination
-        autoplay={{ delay: 3000 }}
-        scrollbar={{ draggable: true }}
-        //   breakpoints={{
-        //     300: {
-        //       slidesPerView: 1,
-        //     },
-        //     600: {
-        //       slidesPerView: 2,
-        //     },
-        //     1024: {
-        //       slidesPerView: 3,
-        //     },
-        //     1280: {
-        //       slidesPerView: 4,
-        //     },
-        //   }}
-      >
-        <SwiperSlide>
-          <div className="flex justify-center items-center">
-            <Image
-              src={"/images/slider/slide1.jpg"}
-              // objectFit="cover"
-              className="rounded-lg"
-              width={1320}
-              height={310}
-              alt="alt"
-              // layout="fixed"
-            />
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="flex justify-center items-center">
-            <Image
-              src={"/images/slider/slide2.jpg"}
-              // objectFit="cover"
-              className="rounded-lg"
-              width={1320}
-              height={310}
-              alt="alt"
-            />
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="flex justify-center items-center">
-            <Image
-              src={"/images/slider/slide3.jpg"}
-              // objectFit="cover"
-              className="rounded-lg"
-              width={1320}
-              height={310}
-              alt="alt"
-            />
-          </div>
-        </SwiperSlide>
-      </Swiper>
+    <section className="container mx-auto flex flex-col gap-40 relative p-2 mt-20">
+      <div className="z-30 btns absolute left-4 top-4 flex gap-1">
+        <FaChevronRight
+          onClick={() => {
+            setslideHandler(0);
+            setTimeout(() => {
+              sliderChangingHandler(-1);
+              setslideHandler(1);
+            }, 1000);
+          }}
+          className="bg-white w-10 h-10 p-2 rounded border-zinc-800 border-[.2rem] cursor-pointer hover:border-zinc-500 transition-all duration-500"
+        />
+
+        <FaChevronLeft
+          onClick={() => {
+            setslideHandler(0);
+            setTimeout(() => {
+              sliderChangingHandler(1);
+              setslideHandler(1);
+            }, 1000);
+          }}
+          className="bg-white w-10 h-10 p-2 rounded border-zinc-800 border-[.2rem] cursor-pointer hover:border-zinc-500 transition-all duration-500"
+        />
+      </div>
+      <div className="z-20 flex justify-center items-center gap-6">
+        <Image
+          width={1280}
+          height={300}
+          className={
+            slideHandler == 1
+              ? "rounded-xl animate__animated  animate__bounceIn animate__slow"
+              : "rounded-xl animate__animated  animate__bounceOut animate__slow"
+          }
+          objectFit="cover"
+          layout="fixed"
+          alt={data[nowSlide].title}
+          src={data[nowSlide].img}
+        />
+      </div>
     </section>
   );
 };
 
-export default SliderFour;
+export default MainSlider;
