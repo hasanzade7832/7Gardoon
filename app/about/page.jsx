801b1584fe76c957107e23fee16313c504119b5b
mohammad <hasanzade.hoseyn27@gmail.com>
2024-05-05 14:00:01 +0330

    test

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,7 +1,7 @@
 const Page = () => {
     return(
        <section className="container mx-auto p-12 flex justify-center items-center">
-        <div className="px-8 py-4 rounded-md bg-orange-500 text-white" >درباره ما</div>
+        <div className="px-8 py-4 rounded-md bg-orange-500 text-white" >درباره ما  a</div>
        </section>
     )
 };
